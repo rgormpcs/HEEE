67804479a1a820375f1b894d74bee330785bde5a
rgormpcs <ricardo.mpcs@gmail.com>
2017-08-30 18:49:02 -0500

    estudio amputaciones

--- target/generated-sources/annotations/Model/Entity/Hospital_.java
+++ target/generated-sources/annotations/Model/Entity/Hospital_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2017-08-28T23:11:17")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2017-08-29T14:21:16")
 @StaticMetamodel(Hospital.class)
 public class Hospital_ { 
 
